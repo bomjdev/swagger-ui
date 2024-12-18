714d4e20a8dbc8fec7100e5985159fef1396b754
anon <anon@local>
2024-12-18 07:00:20 -0300

    unpkg bundle

---
+++ register.go
@@ -0,0 +1,25 @@
+package swagger_ui
+
+import (
+	_ "embed"
+	"github.com/labstack/echo/v4"
+	"net/http"
+
+	"github.com/getkin/kin-openapi/openapi3"
+)
+
+func Register(e *echo.Echo, spec *openapi3.T) {
+	e.GET("/spec.json", specHandlerFactory(spec))
+	e.GET("/docs", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, swaggerHTML)
+	})
+}
+
+func specHandlerFactory(spec *openapi3.T) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.JSON(http.StatusOK, spec)
+	}
+}
+
+//go:embed index.html
+var swaggerHTML string
